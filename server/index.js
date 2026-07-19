15a2f45efc91f7613b429db586d0e3a283fe2015
sagar kumar mathuri <sagarku2002@gmail.com>
2026-07-19 20:18:46 +0530

    did some changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
 
-const PORT = 5000;
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.error('Error connecting to MongoDB:', error));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
